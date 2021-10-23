0bdff8ca5b9c48c33422d2ef8629367c6666e2c0
Schell Carl Scivally <efsubenovex@gmail.com>
2021-10-23 14:08:13 +1300

    Update main.tf

--- modules/domain_cert/main.tf
+++ modules/domain_cert/main.tf
@@ -39,9 +39,9 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "cert_validation" {
   provider                = aws.virginia
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
   timeouts {
-    create = "120m"
+    create = "2h"
   }
   lifecycle {
     ignore_changes = [id]
